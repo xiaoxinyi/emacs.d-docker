e91aa96ffbb81986ddd960fde91814b5720ccda9
root <xiaoxin88214@163.com>
2016-11-02 12:50:39 +0000

    hs-minor-mode for code folding

--- super-emacs/packages.el
+++ super-emacs/packages.el
@@ -429,3 +429,29 @@
   (global-set-key (kbd "M-.") 'my-find-tag)
 
   )
+
+;; hs-minor-mode
+(defun toggle-selective-display (column)
+  (interactive "P")
+  (set-selective-display
+   (or column
+       (unless selective-display
+         (1+ (current-column))))))
+
+(defun toggle-hiding (column)
+      (interactive "P")
+      (if hs-minor-mode
+          (if (condition-case nil
+                  (hs-toggle-hiding)
+                (error t))
+              (hs-show-all))
+        (toggle-selective-display column)))
+
+(load-library "hideshow")
+(global-set-key (kbd "C-+") 'toggle-hiding)
+(global-set-key (kbd "C-\\") 'toggle-selective-display)
+
+(add-hook 'c-mode-common-hook   'hs-minor-mode)
+(add-hook 'emacs-lisp-mode-hook 'hs-minor-mode)
+;; (add-hook 'elpy-mode-hook 'hs-minor-mode)
+(add-hook 'lua-mode-hook 'hs-minor-mode)
